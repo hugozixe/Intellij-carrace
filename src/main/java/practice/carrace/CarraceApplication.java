93a0491d7c72314196e7303fdd7ee70778b1b86e
Tae Hee Lee <liver1379@naver.com>
2024-06-05 10:45:01 +0900

    Initial Commit

---
+++ src/main/java/practice/carrace/CarraceApplication.java
@@ -0,0 +1,13 @@
+package practice.carrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarraceApplication {
+
+	public static void main(String[] args){
+
+	}
+
+}
---
+++ src/test/java/practice/carrace/CarraceApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.carrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
